510787ecb4f18f72676dd94b58ac8d3e0a7cd704
Neil Richardson <nrich@ii.net>
2016-03-28 07:41:48 +0800

    rewrite fetcher to multi process to speed up significatly; grab missing articles upfront then fetch; optioanlly fill articles missing from server with dummy data

--- fill_missing_articles.pl
+++ fill_missing_articles.pl
@@ -10,8 +10,19 @@ use POSIX qw/:sys_wait_h strftime/;
 use lib qw(lib);
 use UsenetIndexer qw//;
 
+my %CHILDREN = ();
+
+sub REAPER {
+    while ((my $child = waitpid(-1, WNOHANG)) > 0) {
+        print("Reaping child $child\n");
+        delete $CHILDREN{$child};
+    }
+
+    $SIG{CHLD} = \&REAPER;
+}
+
 my %opts = ();
-getopts('hc:', \%opts);
+getopts('fhc:', \%opts);
 $opts{h} and usage();
 main(@ARGV);
 
@@ -22,6 +33,8 @@ sub main {
 
     my $config = $opts{c} || 'etc/common.conf';
 
+    my $fill_missing = $opts{f} ? 1 : 0;
+
     my $dbh = UsenetIndexer::GetDB($config, AutoCommit=>1);
 
     my $sth = $dbh->prepare('SELECT MIN(article), MAX(article) FROM usenet_article');
@@ -31,16 +44,57 @@ sub main {
 
     my $middle = int(($max-$min)/2);
 
-
     my $newsgroup_id = UsenetIndexer::GetNewsGroupID($dbh, $newsgroup);
-    my $nntp = UsenetIndexer::GetNNTP($config);
-    $nntp->group($newsgroup);
 
-    missing_between($nntp, $dbh, $min, $max, $newsgroup_id);
+    my @missing = ();
+    missing_between($dbh, $min, $max, $newsgroup_id);
+
+    $dbh->disconnect();
+
+    $SIG{CHLD} = \&REAPER;
+
+    my @work = ();
+    while (my $article_id = shift @missing) {
+        push @work, $article_id;
+
+        if (scalar @work >= 100) {
+            my $pid = fork();
+
+            if ($pid) {
+                @work = ();
+                $CHILDREN{$pid} = 1;
+
+                while (scalar keys %CHILDREN >= 10) {
+                    sleep 1;
+                }
+            } elsif (defined $pid) {
+                srand();
+                my $dbh = UsenetIndexer::GetDB($config, AutoCommit=>1);
+                my $nntp = UsenetIndexer::GetNNTP($config);
+                $nntp->group($newsgroup);
+
+                get_article($nntp, $dbh, $_, $newsgroup_id, $fill_missing) for @work;
+                exit 0;
+            } else {
+                die "Fork failed: $!\n";
+            }
+        }
+    }
+
+    if (@work) {
+        my $dbh = UsenetIndexer::GetDB($config, AutoCommit=>1);
+        my $nntp = UsenetIndexer::GetNNTP($config);
+        $nntp->group($newsgroup);
+        get_article($nntp, $dbh, $_, $newsgroup_id, $fill_missing) for @work;
+    }
+
+    while (%CHILDREN) {
+        sleep 1;
+    }
 }
 
 sub missing_between {
-    my ($nntp, $dbh, $min, $max, $newsgroup_id) = @_;
+    my ($dbh, $min, $max, $newsgroup_id, $missing) = @_;
 
     my $sth = $dbh->prepare('SELECT COUNT(1) FROM usenet_article WHERE article >= ? AND article <= ?');
     $sth->execute($min, $max);
@@ -48,20 +102,14 @@ sub missing_between {
 
     if ($max-$min <= 200) {
         if ($count < $max-$min) {
-            my @missing = ();
-
-            print STDERR "Looking for gap in $min -> $max (count is $count)\n";
-
             for my $article ($min .. $max) {
                 my $exists = $dbh->prepare('SELECT COUNT(1) FROM usenet_article WHERE article = ?');
                 $exists->execute($article);
                 my ($article_exists) = $exists->fetchrow_array();
                 $exists->finish();
 
-                push @missing, $article unless $article_exists;
+                push @$missing, $article unless $article_exists;
             }
-
-            get($nntp, $dbh, $_, $newsgroup_id) for @missing;
         }
         return;
     }
@@ -69,17 +117,27 @@ sub missing_between {
     if ($count != ($max-$min)) {
         my $middle = int(($max-$min)/2);
 
-        missing_between($nntp, $dbh, $min, $min+$middle, $newsgroup_id);
-        missing_between($nntp, $dbh, $min+$middle, $max, $newsgroup_id);
+        missing_between($dbh, $min, $min+$middle, $newsgroup_id, $missing);
+        missing_between($dbh, $min+$middle, $max, $newsgroup_id, $missing);
     }
 }
 
-sub get {
-    my ($nntp, $dbh, $article_id, $newsgroup_id) = @_;
+sub get_article {
+    my ($nntp, $dbh, $article_id, $newsgroup_id, $fill_missing) = @_;
 
     my $sth = $dbh->prepare('INSERT INTO usenet_article(article,message,subject,posted,newsgroup_id) VALUES(?,?,?,?,?)');
 
     my $article = UsenetIndexer::GetArticle($nntp, $article_id);
+
+    if ($fill_missing) {
+        my $message = '_' x 254;
+        my @tokens = ('A' .. 'Z', 'a' .. 'z', '0' .. '9');
+        $message =~ s/_/$tokens[rand @tokens]/ge;
+        my $posted = strftime '%Y-%m-%d %H:%M:%S', localtime();
+
+        $article ||= {message => $message, subject => 'MISSING ARTICLE', posted => $posted};
+    }
+
     return unless $article;
 
     $sth->execute($article_id, $article->{message}, $article->{subject}, $article->{posted}, $newsgroup_id);
@@ -87,8 +145,9 @@ sub get {
 
 sub usage {
     print <<EOF;
-Usage: $0
+Usage: $0 <newsgroup name>
     [-c config file|etc/common.conf]
+    [-f fill missing articles with dummy data]
 EOF
 
     exit 1;
